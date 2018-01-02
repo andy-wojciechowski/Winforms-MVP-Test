48fd7601c69279408f5546446e5f86700253b73e
andy-wojciechowski <wojciechowskia@msoe.edu>
2018-01-01 18:40:40 -0600

    Commented back in the validate password method in the PasswordHashingService due to the .NET standard upgrade.

--- CardCollectorStandard.Domain/Services/PasswordHashingService.cs
+++ CardCollectorStandard.Domain/Services/PasswordHashingService.cs
@@ -11,24 +11,24 @@ namespace CardCollectorStandard.Domain.Services
     {
         public bool ValidatePassword(string password, string savedHash)
         {
-            ////Modeled from https://stackoverflow.com/questions/4181198/how-to-hash-a-password/10402129#10402129
-            ////Create the salt
-            //byte[] salt;
-            ////new RNGCryptoServiceProvider().GetBytes(salt = new byte[16]);
+            //Modeled from https://stackoverflow.com/questions/4181198/how-to-hash-a-password/10402129#10402129
+            //Create the salt
+            byte[] salt;
+            new RNGCryptoServiceProvider().GetBytes(salt = new byte[16]);
 
-            ////Get the hash value
-            //var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 10000);
-            //byte[] hash = pbkdf2.GetBytes(20);
+            //Get the hash value
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 10000);
+            byte[] hash = pbkdf2.GetBytes(20);
 
-            ////Combine the salt and password bytes
-            //byte[] hashBytes = new byte[36];
-            //Array.Copy(salt, 0, hashBytes, 0, 16);
-            //Array.Copy(hash, 0, hashBytes, 16, 20);
+            //Combine the salt and password bytes
+            byte[] hashBytes = new byte[36];
+            Array.Copy(salt, 0, hashBytes, 0, 16);
+            Array.Copy(hash, 0, hashBytes, 16, 20);
 
-            //string computedHash = Convert.ToBase64String(hashBytes);
+            string computedHash = Convert.ToBase64String(hashBytes);
 
-            //if (savedHash.Equals(computedHash))
-            //    return true;
+            if (savedHash.Equals(computedHash))
+                return true;
             return false;
         }
     }
