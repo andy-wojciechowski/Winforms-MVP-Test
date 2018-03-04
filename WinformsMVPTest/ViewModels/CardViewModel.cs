78ca500b888608edc7730a9f31cea7ab57b53731
andy-wojciechowski <wojciechowskia@msoe.edu>
2018-03-04 10:02:54 -0600

    Removed the EditCardCollectionViewModel

---
+++ WinformsMVPTest/ViewModels/CardViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WinformsMVPTest.ViewModels
+{
+	public class CardViewModel
+    {
+        public Guid SetID { get; set; }
+        public string Name { get; set; }
+        public bool IsOwned { get; set; }
+    }
+}
